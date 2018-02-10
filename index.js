7a9cf9c118a16ec76a7a78c5a47918d753bdcc04
rmarro <rmarro47@gmail.com>
2018-02-09 18:37:43 -0600

    created very basic gameplay-- will display blanks, take user input, and display correct letters in word

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-// select random word
-// use word constructor to make new word
-    // make ltrArray
-    // make display word
-// accept user input and store as guess
-// checkguess(guess)
-    // how to tell if guess was correct or not???????
-        // all ltrCheck are false, or one or more are true
-    // if correct, nothing
-    // if wrong, guessesleft-- and push to already guessed array
-// makedisplayword
-// display guessesleft and already guessed array
+var inquirer = require("inquirer");
+var Word = require('./word.js');
+
+var wordOptions = ["dog", "bird", "hedgehog"];
+var currentWord;
+
+// Selects random word, creates array of letter objects, and prompts player
+function startGame() {
+    inquirer.prompt(
+        [
+            {
+                name: "start",
+                type: "select",
+                message: "WELCOME TO GUESS THE ANIMAL \n  PRESS ENTER TO START GAME"
+            }
+        ]
+    ).then(function() {
+        var wrd = wordOptions[Math.floor(Math.random()*3)];
+        currentWord = new Word(wrd);
+        currentWord.makeLtrArray();
+        promptPlayer();
+    });
+};
+
+// Makes display word, takes user guess and checks guess
+function promptPlayer() {
+    currentWord.makeDisplayWord();
+    inquirer.prompt(
+        [
+            {
+            name: "guess",
+            type: "input",
+            message: "Guess a letter!",
+            }
+        ]
+    ).then(function(data) {
+        var guess = data.guess;
+        currentWord.checkGuess(guess);
+        promptPlayer();
+    })
+};
+
+startGame();
+
+
+// TRY THIS LOGIC FOR GAMEPLAY
+// gameplay function
+// function to check if word is done
+// if yes, congrats and inquire play again
+// if no, function to check if guesses left
+// if no, out of guesses and prompt play again
+// if yes, prompt player for guess
+// if correct, gameplay
+// if incorrect, guessesLeft-- and gameplay
+
+
+
+
 
--- word.js
+++ word.js
@@ -1,26 +1,27 @@
 var Letter = require('./letter.js');
 
-var Word = function(wrd) {
-    this.wrdSplit = wrd.split("");
+var Word = function(word) {
+    this.wordSplit = word.split("");
     this.ltrArray = [];
 
     // Use Letter constructor to make an array of Letter objects
     this.makeLtrArray = function() {
-        for (i=0; i < this.wrdSplit.length; i++) {
-            var ltrObject = new Letter(this.wrdSplit[i]);
+        for (i=0; i < this.wordSplit.length; i++) {
+            var ltrObject = new Letter(this.wordSplit[i]);
             this.ltrArray.push(ltrObject);
         };
     };
 
     // Use function in each letter object to display letter or blank
-    this.makeDisplayWrd = function() {
-        this.displayWrdArray = [];
+    this.makeDisplayWord = function() {
+        this.displayWordArray = [];
         for (i=0; i < this.ltrArray.length; i++) {
             var ltrDisplay = this.ltrArray[i].ltrDisplay();
-            this.displayWrdArray.push(ltrDisplay);
+            this.displayWordArray.push(ltrDisplay);
         };
         // Should this be changed to return?
-        console.log(this.displayWrdArray.join(" "));
+        console.log(this.displayWordArray.join(" "));
+        console.log("");
     };
 
     this.checkGuess = function(guess) {
@@ -30,20 +31,21 @@ var Word = function(wrd) {
     };
 };
 
+module.exports = Word;
 
 // TESTING: this all works!!!!!!!
 // Create new word object, call make letter array, call make display word
-wrd = "hedgehog";
-var test = new Word(wrd);
-test.makeLtrArray();
-test.makeDisplayWrd();
+// wrd = "hedgehog";
+// var test = new Word(word);
+// test.makeLtrArray();
+// test.makeDisplayWord();
 
 // make a guess, run check guess, run make display word
-var guess = "h";
-test.checkGuess(guess);
-test.makeDisplayWrd();
+// var guess = "h";
+// test.checkGuess(guess);
+// test.makeDisplayWord();
 
 // make a guess, run check guess, run make display word
-var guess = "e";
-test.checkGuess(guess);
-test.makeDisplayWrd();
+// var guess = "e";
+// test.checkGuess(guess);
+// test.makeDisplayWord();
